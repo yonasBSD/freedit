e3d87791925ac04af2b82306964fceaef14768d3
Yonas <yonas.y@gmail.com>
2024-10-03 18:08:07 -0400

    Fix build on FreeBSD.

--- src/main.rs
+++ src/main.rs
@@ -12,9 +12,9 @@ use tokio::net::TcpListener;
 use tracing::{error, info, warn};
 use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
-#[cfg(not(target_os = "windows"))]
-#[global_allocator]
-static GLOBAL: tikv_jemallocator::Jemalloc = tikv_jemallocator::Jemalloc;
+//#[cfg(not(target_os = "windows"))]
+//#[global_allocator]
+//static GLOBAL: tikv_jemallocator::Jemalloc = tikv_jemallocator::Jemalloc;
 
 #[tokio::main]
 async fn main() -> Result<(), AppError> {
